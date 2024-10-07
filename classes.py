c2c3b3c27250a4df1afd308af4b8af3e5181ac5b
jchesson6 <77857117+jchesson6@users.noreply.github.com>
2024-10-07 09:30:24 -0400

    Add files via upload
    Basic classes for now will update features after creating menus

---
+++ classes.py
@@ -0,0 +1,65 @@
+""" Event would be created on a menu where the user enters the name and people. This information is stored
+in the event class but there are transactions that can also be added after the event has been made.
+Transactions would be created in a menu where the user enters the payer, the debtors, the total amount of the
+transaction, the individual amounts owed by the debtors and a description of the transaction to identify it.
+The account will have to hold information about the amounts owed to and by the user.
+The new friend class will contain the amounts owed to and by friends that the user adds to the app. """
+
+
+
+class Event:
+
+    num_transactions = 0
+    transactions = []
+
+    def __init__(self, name, people):
+        self.name = name
+        self.people = people
+
+    def add_people(self, people):
+        self.people.append(people)
+        # this is basic but needs to be changed to make sure people areent duplicated, etc
+
+    def add_transaction(self, transaction):
+        self.transactions.append(transaction)    
+
+    # need to look into adding iterator mechanics __iter__() and __next__()
+
+
+class Transaction:
+
+    total = 0
+
+
+    def __init__(self, description, payer, debtors):
+        self.description = description
+        self.payer = payer
+        self.debtors = debtors
+
+    def add_debtor(self, debtor):
+        self.debtors.append(debtor)
+
+    # need to look into adding iterator mechanics __iter__() and __next__()
+
+
+class Account:
+
+    num_friends = 0
+    amt_owed_to_acc = 0
+    amt_owed_by_acc = 0
+    amt_owed_total = 0
+
+    def __init__(self, name):
+        self.name = name
+
+
+    # need to look into adding iterator mechanics __iter__() and __next__()
+
+
+class Friend:
+    
+    amount_owed_to_user = 0
+    amount_owed_by_user = 0
+
+    def __init__(self, name):
+        self.name = name
